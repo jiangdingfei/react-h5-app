9e44b6f8cff7d2f2193646b2fbbaccd5ba16f964
jiangdingfei <jdfzangtian@gmail.com>
2019-12-15 21:06:43 +0800

    first commit

---
+++ src/config/envConfig.js
@@ -0,0 +1,21 @@
+// 基于打包命令配置的生产环境和开发环境
+const ENVIRONMENT = process.env.REACT_APP_ENV;
+
+// 基于环境变量(需要在服务器和开发电脑各自添加环境变量)
+// const ENVIRONMENT = process.env.NODE_ENV
+export default getEnv(ENVIRONMENT);
+
+function getEnv(env) {
+  switch (env) {
+    case development:
+      return {
+        baseURL: '',
+        imgSrc: ''
+      }
+    case production:
+      return {
+        baseURL: '',
+        imgSrc: ''
+      }
+  }
+}
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Route,
+  Redirect,
+  HashRouter,
+  Switch
+} from 'react-router-dom';
+import Login from 'pages/Login';
+import Home from 'pages/Home';
+
+export default function routerMap() {
+  return (
+    <HashRouter>
+			<Switch>
+				<Redirect exact from="/" to="/home"><Redirect>
+				<Route path="/login" component={login}></Route>
+				<Route path="/home" component={Home}></Route>
+			</Switch>
+		</HashRouter>
+  )
+}
