4b73cf2a49da825ea23dba23cae385d739011dcf
jiangdingfei <jdfzangtian@gmail.com>
2020-04-22 21:34:59 +0800

    update hooks Component

--- src/pages/HooksDemo/index.tsx
+++ src/pages/HooksDemo/index.tsx
@@ -4,15 +4,7 @@ import { formatSumIns } from '../../utils/index'
 const LIST = ['a111', '24555', '2509', '100', '200', 'b3222', 'd2322', '1223']
 const SUMINS = ['1000', '2000', '5000', '6000', '8000', '12000', '19000', '23000']
 function sortList(list: string[]): string[] {
-  return list.sort((a,b) => {
-    let c: number
-    if(a>b) {
-      c = 1
-    } else {
-      c = -1
-    }
-    return c
-  })
+  return list.sort((a, b) => a > b ? 1 : -1)
 }
 export default function Example() {
   const [count, setCount] = useState(0)
@@ -20,14 +12,14 @@ export default function Example() {
     // console.log(count)
     console.log(sortList(LIST), 'sort')
     console.log(formatSumIns(SUMINS))
-  },[])
+  }, [])
   useEffect(() => {
     document.title = `You clicked ${count} times`
   })
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button  onClick={() => setCount(count +1)}>Add</button>
+      <button onClick={() => setCount(count + 1)}>Add</button>
     </div>
   )
 }
